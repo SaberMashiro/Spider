94f14b28998f25442e6e36ad1de209494ec656c2
SaberMashiro <SaberMashiro@users.noreply.github.com>
2017-10-30 23:52:34 +0800

    bug没修完的第二陀屎

--- 想要去做的/spider_testv0.2.py
+++ 想要去做的/spider_testv0.2.py
@@ -42,8 +42,8 @@ class Spider():
 
     def __init__(self):
         self.url = 'http://www.meizitu.com/'#当前的url
-        self.domain = 'www.meizutu.com'#域名
-        self.urls = []#当前页面所有的url
+        self.domain = 'www.meizitu.com'#域名
+       # self.urls = []#当前页面所有的url
         self.list_url = []#下一级可爬url
         self.list_picture = []#当前页面的图片
         self.bloomfilter = Bloomfilter.BloomFilter()#手写的布隆过滤器,去重用
@@ -53,8 +53,9 @@ class Spider():
         Session = requests.session()
         return Session.get(url).content
 
-    def Get_url_list(self,content=self.Getcontent(self.url),paser=u'//body//a'):
+    def Get_url_list(self,paser=u'//body//a'):
         #返回当前页面的所有非图片链接
+        content=self.Getcontent(self.url)
         page = etree.HTML(content)
         tag = page.xpath(paser)
         urls = []
@@ -62,8 +63,9 @@ class Spider():
             urls.append(i.get('href'))
         return urls
 
-    def Get_all_list(self,content=self.Getcontent(self.url),paser=u'//body//img'):
+    def Get_all_list(self,paser=u'//body//img'):
         #返回当前页面所有的链接
+        content=self.Getcontent(self.url)
         page = etree.HTML(content)
         tag = page.xpath(paser)
         pics = []
@@ -72,42 +74,51 @@ class Spider():
         return self.Get_url_list()+pics
         
     #def Recorrect_url(*urls,*list_url,*list_picture):
-    def  Recorrect_url(self,urls=self.Get_all_list()):
+    def  Recorrect_url(self):
         #将本页面所有的url去重后写入各自该存在的列表
+        urls=self.Get_all_list()
+        #print urls
         for url in urls:
-            if url.netloc not is self.domain:
-                continue
-            else:
-                if not self.bloomfilter.isContain(url):
-                    self.bloomfilter.insert(url) 
-                    if url.path.endswith('jpg'):
-                        self.list_picture.append(url)
-                    else:
-                        self.list_url.append(url)
+            urlp = urlparse.urlparse(url)
+            if not self.bloomfilter.isContains(url):
+                self.bloomfilter.insert(url)
+                if urlp.path.endswith('jpg'):
+                    self.list_picture.append(url)
+                elif urlp.netloc == self.domain:
+                    self.list_url.append(url)
+            
 
-    def Download():
+    def Download(self):
         #将该页面的图片下载，之后进入另一个url
         try:
-            while self.list_picture not null:
+            print 'HAHA'
+            while self.list_picture is not None:
+                print 'HAHA'
                 url = self.list_picture.pop()
+                print 'HAHA'
                 with open('images/{}'.format(urlparse.urlparse(url).path.repladce('/','_')[-8:]),'wb+') as f:
-                f.write(Getcontent(url))
-                f.close()
+                    f.write(Getcontent(url))
+                    f.close()
         except:
             print 'Pop error,Please check whether list is null'
             return False
         
 
 if __name__ == '__main__':
-    if os.path not exists('images'):
+    if not os.path.exists('images'):
         os.mkdir('images')
     spider = Spider()
     try:
         while True:
             spider.Recorrect_url()
-            Download()
-            if spider.list_url not null:
+            print spider.list_url
+            print '------************-------'
+            print spider.list_picture
+            spider.Download()
+            if spider.list_url is not None:
                 spider.url = spider.list_url.pop()
             else:
                 print 'All is down'
                 sys.exit(0)
+    except:
+        print 'Something wrong when running'
